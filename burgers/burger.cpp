a2d0dea97cda07744413bf9ffd566e9abed4d651
Morgan Quigley <morgan@osrfoundation.org>
2016-05-24 17:28:57 -0700

    fix issue #3 by writing custom hacky base64 decoder

--- burgers/burger.cpp
+++ burgers/burger.cpp
@@ -1,18 +1,60 @@
 #include "burger.h"
-#include "b64/decode.h"
 #include <cstring>
 #include <stdio.h>
 using namespace burger; // i've always wanted to write that
 
 static const char *burger_cstr;
 
+// here lies the world's slowest portable base64 decoder
+void decode_base64(const char *cstr, std::vector<uint8_t> &out)
+{
+  int len = (int)strlen(cstr);
+  if (len < 2)
+    return; // would have to think too hard about trivial inputs
+  out.resize(len * 3 / 4); // deal with padding bytes later
+  uint8_t base64_map[256] = {0};
+  for (int i = 'A'; i <= 'Z'; i++)
+    base64_map[i] = i - 'A';
+  for (int i = 'a'; i <= 'z'; i++)
+    base64_map[i] = i - 'a' + 26;
+  for (int i = '0'; i <= '9'; i++)
+    base64_map[i] = i - '0' + 52;
+  base64_map['+'] = 62;
+  base64_map['/'] = 63;
+  int ridx = 0, widx = 0;
+  uint32_t block;
+  for (ridx = 0; ridx < len; ridx += 4)
+  {
+    // we expand each group of 4 code bytes into 3 output bytes
+    uint32_t block = 0;
+    block = (base64_map[cstr[ridx  ]] << 18) |
+            (base64_map[cstr[ridx+1]] << 12) |
+            (base64_map[cstr[ridx+2]] <<  6) |
+            (base64_map[cstr[ridx+3]] <<  0) ;
+    out[widx++] = (block >> 16) & 0xff;
+    out[widx++] = (block >>  8) & 0xff;
+    out[widx++] = (block >>  0) & 0xff;
+  }
+  // fix padding now. (these branches are untested so they're probably wrong)
+  if (cstr[len-1] == '=' && cstr[len-2] == '=')
+  {
+    // there were two padding bytes. remove the two last output bytes
+    out.pop_back();
+    out.pop_back();
+  }
+  else if (cstr[len-1] == '=')
+  {
+    // there was only one padding byte. remove the last output byte.
+    out.pop_back();
+  }
+}
+
 Burger::Burger()
 {
-  base64::decoder D;
   size_t burger_size = strlen(BURGER);
-  std::vector<char> burger_png;
+  std::vector<uint8_t> burger_png;
   burger_png.resize(burger_size);
-  D.decode(BURGER, strlen(BURGER), burger_png.data());
+  decode_base64(BURGER, burger_png);
   burger_template = cv::imdecode(burger_png, CV_LOAD_IMAGE_COLOR);
   srand(time(NULL));
 }
